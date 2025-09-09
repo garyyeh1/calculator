bb9edc023e3c49e3061d0927a37b9f3f413fe8c7
garyyeh1 <123456789+odin@users.noreply.github.com>
2025-09-09 16:09:50 +0100

    Working on the operations

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # calculator
 
 A Project showcasing my JavaScript logic and CSS design.
+
+Calculator display
+- Instead of appending the numbers for every button clicked, I concatenate the numbers. Gives the same result on display, but the former will not be able to be used for different operations.
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+// button animation
 let numbers = document.querySelectorAll(".numbers button")
 
 numbers.forEach(btn => btn.addEventListener('mouseover', myFunc));
@@ -27,4 +27,36 @@ operations.forEach(btn=> btn.addEventListener('mouseout', myFunc4));
 function myFunc4(){
     this.style.backgroundColor = '';
     this.style.transform = '';
+}
+
+//making button work when clicked
+let buttons = document.querySelectorAll(".numbers button")
+let display = document.querySelector(".display")
+
+buttons.forEach(btn=> btn.addEventListener('click', display1));
+function display1(){
+    if (display.textContent == 0){    
+        display.textContent = this.innerHTML
+    } else{
+        display.textContent += this.textContent
+    }
+}
+
+//operations
+let operation = document.querySelectorAll(".operations button")
+
+operation.forEach(btn=> btn.addEventListener('click', operating));
+function operating(){
+    if (operation.textContent ==='+'){    
+        const num1 = display.textContent
+        this.textContent.replace(display.textContent)
+
+    } else if (operation.textContent == '-') {
+        
+    } else if (operation.textContent== 'x'){
+        //multiply
+    } else {
+        //divide
+    }
+
 }
