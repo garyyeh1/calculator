232ebe4581f75719f4feb4067d35be6b5fd02912
garyyeh1 <123456789+odin@users.noreply.github.com>
2025-09-10 01:44:38 +0100

    Single-flow operations are working

--- index.js
+++ index.js
@@ -29,34 +29,75 @@ function myFunc4(){
     this.style.transform = '';
 }
 
+
+let resetDisplay = false
+let num1 = 0
+let num2 = 0
+let operator = null
+result = 0
 //making button work when clicked
-let buttons = document.querySelectorAll(".numbers button")
+let buttons = document.querySelectorAll(".numbers button:not(#result)")
 let display = document.querySelector(".display")
 
 buttons.forEach(btn=> btn.addEventListener('click', display1));
 function display1(){
-    if (display.textContent == 0){    
-        display.textContent = this.innerHTML
-    } else{
+    if (display.textContent == ('0')){    
+        display.textContent = this.textContent
+    }else if (resetDisplay == true){
+        display.textContent = this.textContent
+        resetDisplay= false
+    }
+    else{
         display.textContent += this.textContent
     }
+
 }
+
+
 
 //operations
 let operation = document.querySelectorAll(".operations button")
 
 operation.forEach(btn=> btn.addEventListener('click', operating));
 function operating(){
-    if (operation.textContent ==='+'){    
-        const num1 = display.textContent
-        this.textContent.replace(display.textContent)
+        num1 = Number(display.textContent)
+    if (this.textContent =='+'){    
+        display.textContent = '+'
+        operator = '+'
+        resetDisplay = true
 
-    } else if (operation.textContent == '-') {
-        
-    } else if (operation.textContent== 'x'){
-        //multiply
-    } else {
-        //divide
+    } else if (this.textContent == '-') {
+        display.textContent = '-'
+        operator = '-'
+        resetDisplay = true
+    } else if (this.textContent == 'x'){
+        display.textContent = 'x'
+        operator = 'x'
+        resetDisplay = true    
+    } else if (this.textContent == '/'){
+        display.textContent = '/'
+        operator = '/'
+        resetDisplay = true
     }
 
+}
+
+document.querySelector("#result").addEventListener("click", equals);
+
+function equals(){
+    num2 = Number(display.textContent)
+    console.log(num1, num2, operator)
+    if (operator === '+'){
+        result = num1 + num2
+        display.textContent = result
+    }else if (operator === '-'){
+        result = num1 - num2
+        display.textContent = result
+    }else if (operator ==='x'){
+        result = num1*num2
+        display.textContent = result
+    }else if (operator === '/'){
+        result = num1/num2
+        display.textContent = result
+    }
 }
